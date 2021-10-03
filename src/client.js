006343397f9550341d9ad3f72419883bb582dad7
Altino Alves Júnior <44076017+altinoalvesjunior@users.noreply.github.com>
2021-10-03 12:48:41 -0300

    including getAllKeys

--- src/client.js
+++ src/client.js
@@ -2,61 +2,71 @@ const grpc = require("grpc");
 const path = require("path");
 
 const KeyValue = grpc.load(path.resolve(__dirname, "./notes.proto")).KeyValue;
-
-const client = new KeyValue (
-    "localhost:40073",
-    grpc.credentials.createInsecure()
-);
+const client = new KeyValue("localhost:40073", grpc.credentials.createInsecure());
 
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 menu();
 
 rl.question("> ", (string) => {
     const line = string.split(" ")[0];
-	
+
     const remaining = string.split(" ");
-	remaining.shift();
+    remaining.shift();
 
-	const remainingString = remaining.join("");
-
-   // do {
-        switch (line) {
-
-            case "put":
-                put(remainingString);
+    const remainingString = remaining.join("");
+    switch (line) {
+        case "getAllKeys":
+            getAllKeys();
             break;
 
-            case "help":
-                menu();
-            break;
-            
-            case "exit":
-                console.log("Finalizando o programa!");
+        case "put":
+            put(remainingString);
             break;
 
-            default:
-                console.log("O comando digitado não existe.");
-        }
-   // } while (command != "exit")
-	
-	rl.close();
+        case "help":
+            menu();
+            break;
+
+        case "exit":
+            console.log("Finalizando o programa!");
+            break;
+
+        default:
+            console.log("O comando digitado não existe.");
+    }
+
+    rl.close();
 });
 
-function menu () {
-    console.log("-------------- MENU --------------");
-    console.log(" 1 - Para inserir nova chave e valor, digite 'put chave:valor' \n 2 - Para listar alguma chave existente em específico, digite 'get nomeChave' \n 3 - Para listar todas as chaves existente, digite 'getAllKeys'")
-    console.log("----------------------------------");
+function getAllKeys() {
+    client.getAllKeys({}, (error, res) => {
+
+        if (!(error)) {
+            const keyss = res.keys?.map(
+                (item) => item.key)?.join(" | ");
+
+            console.log(`Todas as chaves: ${keyss}`);
+        } else {
+            console.log(error);
+        }
+    });
 }
 
 function put(string) {
-	const key = string.split(":")[0];
-	const value = string.split(":")[1];
+    const key = string.split(":")[0];
+    const value = string.split(":")[1];
 
-	client.put({ key, value }, (error, _) => error ? console.log(error) : console.log("Cadastro realizado com sucesso!"));
+    client.put({ key, value }, (error, _) => error ? console.log(error) : console.log("Cadastro realizado com sucesso!"));
+}
+
+function menu() {
+    console.log("-------------- MENU --------------");
+    console.log(" 1 - Para inserir nova chave e valor, digite 'put chave:valor' \n 2 - Para listar alguma chave existente em específico, digite 'get nomeChave' \n 3 - Para listar todas as chaves existente, digite 'getAllKeys'")
+    console.log("----------------------------------");
 }
