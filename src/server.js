76a8efbbd28c8e3d33dd2f8a7d21e898a5fbcc67
Altino Alves Júnior <44076017+altinoalvesjunior@users.noreply.github.com>
2021-10-03 09:45:04 -0300

    creating project (packages,  .proto) and server finished

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const grpc = require("grpc");
+const path = require("path");
+
+const items = grpc.load(path.resolve(__dirname, "./notes.proto"));
+
+const server = new grpc.Server();
+
+const map = new Map();
+
+server.addService(items.KeyValue.service, {
+
+    get: (call, callback) => {
+		const key = call.request.key;
+		console.log(key);
+
+		if (map.has(key)) callback(null, { value: map.get(key),});
+        else callback({ code: grpc.status.NOT_FOUND, details: "O item buscado, não foi encontrado!", });
+	},
+
+    getAllKeys: (_, callback) => {
+        const keys = [...map.keys()].map((string) => ({
+			key: string,
+		}));
+
+		callback(null, keys);
+	},
+	
+    put: (call, callback) => {
+		const item = call.request;
+		map.set(item.key, item.value);
+
+		callback(null, {});
+	},
+});
+
+server.bind("127.0.0.1:40073", grpc.ServerCredentials.createInsecure());
+
+console.log("O servidor está sendo executado no endereço: 127.0.0.1:40073");
+server.start();
